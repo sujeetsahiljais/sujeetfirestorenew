5920fa64f2fcde931193d931185f90a016f4ddb4
Sujeet Kumar Jaiswal <49628355+sujeetsahiljais@users.noreply.github.com>
2020-10-25 00:20:43 +0530

    Add files via upload

---
+++ sujeetfirestorenew/Firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBTOaRktOqedJ_C8ie2qHwNG68C7zGNQ_s",
+    authDomain: "sujeet-firestore-auth.firebaseapp.com",
+    databaseURL: "https://sujeet-firestore-auth.firebaseio.com",
+    projectId: "sujeet-firestore-auth",
+    storageBucket: "sujeet-firestore-auth.appspot.com",
+    messagingSenderId: "631698420627",
+    appId: "1:631698420627:web:4232be5f44c524c9adee66",
+    measurementId: "G-CCB3Z2X0VW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
